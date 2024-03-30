f4eda4266e24c7eaa690d71a714c9273479f1e96
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-03-30 19:19:41 +0530

    added test cases to validate email address for valid and invalid email format

--- src/main/java/com/creditcard/domain/model/validator/CustomerEmailIdValidator.java
+++ src/main/java/com/creditcard/domain/model/validator/CustomerEmailIdValidator.java
@@ -6,8 +6,9 @@ import java.util.regex.Pattern;
 
 public class CustomerEmailIdValidator {
     public static boolean isValidEmail(String customerEmail) throws InvalidCustomerEmailException {
-        if (!(Pattern.matches("^[^@]+@[^@.]+\\\\.[^@.]+$",customerEmail)))
-            throw new InvalidCustomerEmailException(customerEmail);
-        return true;
+        if ((Pattern.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", customerEmail)))
+            return true;
+        else
+            return false;
     }
 }
